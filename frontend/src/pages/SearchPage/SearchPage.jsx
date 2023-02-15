5ccd882c3cd557ad39d108cf7389cb67541dd856
voolyvex <1joeknapp@gmail.com>
2023-02-15 16:35:08 -0600

    Add SearchPage component and route to App.js

---
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+const SearchPage = () => {
+
+    // async function getSearchResults(key){
+    //     await axios.get("https://www.googleapis.com/youtube/v3/search?q=&part=snippet&type=video&maxResults=3", key=KEY)
+    // };
+    return (
+        <div>
+            <h1>Start your search here</h1>
+        </div>
+    )
+}
+export default SearchPage;
